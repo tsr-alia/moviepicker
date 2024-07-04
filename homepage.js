e1a78299bdd8fe040ad273c1925b31750c6eac96
tsr-alia <kontakt@annakliemann.net>
2024-07-04 12:34:31 +0200

    changed data structure in moviepicker.json, updated draft for movie picker functionality (added query string and changed structure of functions)

--- homepage.js
+++ homepage.js
@@ -79,7 +79,8 @@ function populateGenres(data) {
                   return 0;
                 });
         let elGenreList = document.querySelector("#genreList");
-        let content = "";
+        let content = `<p><input type="checkbox" id="anyGenre" name="genre" value="anyGenre" data-id="genres">
+                <label for="anyGenre">Any Genre!</label><br></p>`;
         for (let genre of genres) {
             content += 
             `<p><input type="checkbox" id="${genre.id}" name="genre" value="${genre.id}" data-id="genres">
@@ -94,8 +95,16 @@ function populateGenres(data) {
         // });
 }
 
-// process form data
-function grabFormData(data) {
+// MOVIE PICKER
+function pickAMovie() {
+    let formData = grabFormData();
+    let queryURL = createQueryString(formData);
+    let movie = getData(queryURL, (data) => filterMovies(data, formData));
+    showMovie(movie);
+    console.log(movie);
+}
+// grabFormData
+function grabFormData() {
     let mood = formMoviePicker.querySelector('input[name="mood"]:checked').value;
     let occasion = formMoviePicker.querySelector('input[name="occasion"]:checked').value;
     let genres = [];
@@ -111,66 +120,131 @@ function grabFormData(data) {
     for (let tag of formMoviePicker.querySelectorAll('input[name="tag"]:checked')) {
         additionalTags.push(tag.value);
     };
-    console.log(mood, occasion, genres, releaseYear, streaming, additionalTags)
-;
-
-    let result = data.filter((movie) => {
-        // tbd "pt" is going to be dynamic when more streaming availability data available
-        
-        let matchStreaming = false;
-        if (movie.streamingOptions.pt !== undefined) {
-            matchStreaming = movie.streamingOptions.pt.some( (item) => {
-                return streaming.includes(item.service.id);
-                }
-            );
-        }
-        let matchMood = movie.mood.includes(mood);
-        let matchOccasion = movie.occasion.includes(occasion);
-        let matchGenre = movie.genres.some( (item) => {
-                return genres.includes(item.id);
-                }
-            );
-        console.log(matchGenre);
-
-        let matchYear = false;
-        matchYear = matchReleaseYear(releaseYear, movie);
-
-        let matchTags = false;
-        if (additionalTags.length > 0 && movie.additionalTags.length > 0) {
-            let matchTags = movie.additionalTags.some( (item) => {
-                return additionalTags.includes(item);
-                }
-            );
-        }
-        console.log(movie.title);
-        return (
-            (matchStreaming && matchMood && matchOccasion && matchGenre && matchYear && matchTags) ||
-            (matchStreaming && matchMood && matchOccasion && matchGenre && matchYear) ||
-            (matchStreaming && matchMood && matchOccasion && matchGenre) ||
-            (matchStreaming && matchMood && matchOccasion) ||
-            (matchStreaming && matchMood)
-        );
-    })
-    console.log(result);
     
-
+    let formData = {
+        streaming: [...streaming],
+        mood: mood,
+        occasion: occasion,
+        genres: [...genres],
+        releaseYear: releaseYear,
+        additionalTags: additionalTags 
+    };
+    return formData;
 }
+
+function createQueryString(formData) {
+    // tbd dynamically set streaming location
+    let streamingLocation = "de";
+    let streamingQuery = formData.streaming.join("|"); 
+    let queryURL = urlShows + `?streaming_${streamingLocation}_like=(${streamingQuery})&mood_like=${formData.mood}&occasion_like=${formData.occasion}`;
+    
+    return queryURL;
+}
+
+function filterMovies(movieData, formData) {
+    console.log("next function:", movieData);
+    let result = [...movieData];
+    let shuffledData = movieData.sort((a, b) => 0.5 - Math.random());
+    let oldResult = [];
+    if (formData.genres.length > 0 && !formData.genres.includes("anyGenre")) {
+        oldResult = [...result];
+        let shuffledGenres = formData.genres.sort((a, b) => 0.5 - Math.random());
+        result = shuffledData.filter( (movie) => {
+            movie.genreList.some( (item) => {
+                return shuffledGenres.includes(item);
+            });
+        });
+        if (result.length === 0) {
+            result = [...oldResult];
+        }    
+    }
+    if (result.length > 1) {
+        oldResult = [...result];
+        result = movieData.filter( (movie) => {
+            matchReleaseYear(formData.releaseYear, movie);
+        });
+        if (result.length === 0) {
+            result = [...oldResult];
+        }    
+    }
+
+    if (result.length > 1 && formData.additionalTags.length > 0) {
+        let shuffledTags = formData.additionalTags.sort((a, b) => 0.5 - Math.random());
+        oldResult = [...result];
+        result = shuffledData.filter( (movie) => {
+            movie.additionalTags.some( (item) => {
+                return shuffledTags.includes(item);
+            });
+        });
+        if (result.length === 0) {
+            result = [...oldResult];
+        }    
+    }  
+    result.sort((a, b) => 0.5 - Math.random());
+    return result[0];
+}
+
+    // let result = data.filter((movie) => {
+    //     // tbd "pt" is going to be dynamic when more streaming availability data available
+        
+    //     let matchStreaming = false;
+    //     if (movie.streamingOptions.pt !== undefined) {
+    //         matchStreaming = movie.streamingOptions.pt.some( (item) => {
+    //             return streaming.includes(item.service.id);
+    //             }
+    //         );
+    //     }
+    //     let matchMood = movie.mood.includes(mood);
+    //     let matchOccasion = movie.occasion.includes(occasion);
+    //     let matchGenre = movie.genres.some( (item) => {
+    //             return genres.includes(item.id);
+    //             }
+    //         );
+    //     console.log(matchGenre);
+
+    //     let matchYear = false;
+    //     matchYear = matchReleaseYear(releaseYear, movie);
+
+    //     let matchTags = false;
+    //     if (additionalTags.length > 0 && movie.additionalTags.length > 0) {
+    //         let matchTags = movie.additionalTags.some( (item) => {
+    //             return additionalTags.includes(item);
+    //             }
+    //         );
+    //     }
+    //     console.log(movie.title);
+    //     return (
+    //         (matchStreaming && matchMood && matchOccasion && matchGenre && matchYear && matchTags) ||
+    //         (matchStreaming && matchMood && matchOccasion && matchGenre && matchYear) ||
+    //         (matchStreaming && matchMood && matchOccasion && matchGenre) ||
+    //         (matchStreaming && matchMood && matchOccasion) ||
+    //         (matchStreaming && matchMood)
+    //     );
+    // })
+    // console.log(result);
 
 function matchReleaseYear(releaseYear, movie) {
     console.log(releaseYear);
     if (releaseYear !== "noRestriction") {
-        if (releaseYear === "lastTwenty" && movie.releaseYear >= (currentYear - 20)) {
+        if (movie.releaseYear >= (currentYear - releaseYear)) {
             return true;
-        } else if (releaseYear === "lastTen" && movie.releaseYear >= (currentYear - 10)) {
-            return true;
-        } else if (releaseYear === "lastFive" && movie.releaseYear >= (currentYear - 5)) {
-            return true;
-        } else {
+        
+        // if (releaseYear === "lastTwenty" && movie.releaseYear >= (currentYear - 20)) {
+        //     return true;
+        // } else if (releaseYear === "lastTen" && movie.releaseYear >= (currentYear - 10)) {
+        //     return true;
+        // } else if (releaseYear === "lastFive" && movie.releaseYear >= (currentYear - 5)) {
+        //     return true;
+         } else {
             return false;
         }
     } else {
         return true;
     }
+}
+
+function showMovie(movie) {
+    
 }
 
 getData(urlShows, populateGenres);
@@ -183,10 +257,19 @@ for (let bt of btNext) {
     bt.addEventListener("click", showNextSection);
 }
 
-btSendMovieForm.addEventListener("click", () => getData(urlShows, grabFormData));
+// btSendMovieForm.addEventListener("click", () => getData(urlShows, grabFormData));
+btSendMovieForm.addEventListener("click", grabFormData);
 
+// activate form and populate with diffrent values .value = ""
+//  alternative: autofill (google extension)
+formMoviePicker.querySelector('input[value="netflix"]').checked = true;
+formMoviePicker.querySelector('input[value="prime"]').checked = true;
+formMoviePicker.querySelector('input[value="happy"]').checked = true;
+formMoviePicker.querySelector('input[value="friends"]').checked = true;
+// formMoviePicker.querySelector('input[value="drama"]').checked = true;
+formMoviePicker.querySelector('input[value="20"]').checked = true;
 
-
-
+pickAMovie();
+// getData(urlShows, grabFormData);
 
 
